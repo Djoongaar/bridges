642dca523a0afa80e1786aa28aed8014e10699a3
evgeny <djoongaar@gmail.com>
2019-11-28 13:36:30 +0300

    YandexMap upgrading

--- bridges/bridges/settings.py
+++ bridges/bridges/settings.py
@@ -7,7 +7,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = ''
+SECRET_KEY = '4dgu^^*xiq-=qrxw9lis2zka9hkj$*+)8lr9ps4lm=(=ikv+zg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
