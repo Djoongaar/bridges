d67b82f1ce7e215f933a9521c7844e8f8c42f02e
evgeny <djoongaar@gmail.com>
2019-12-13 13:37:16 +0300

    comment.user.avatar bug fixed

---
+++ bridges/loaddata.bat
@@ -0,0 +1,8 @@
+python manage.py reset_schema
+python manage.py migrate
+python manage.py loaddata authapp_db.json
+python manage.py loaddata servicesapp_db.json
+python manage.py loaddata productsapp_db.json
+python manage.py loaddata projectsapp_db.json
+python manage.py loaddata researchapp_db.json
+pause
